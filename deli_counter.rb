1e226bf2a0cefe34bf33d5887c26ae0fae9699da
a lan <bluenowte97@gmail.com>
2018-11-06 03:52:48 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,35 @@
-# Write your code here.
-def line(people)
-  if people.length == 0
-    puts "The line is currently empty."
+def line (array)
 
-  people.each do.with_index(1) do |name, index|
-    string = "The line is currently #{index}. #{name}",
+  if array.length == 0
+    puts "The line is currently empty."
+  
+
+  else string = "The line is currently: "
+
+  array.each.with_index(1) do |people,index|
+    string+= "#{index}. #{people} "
   end
-   string = string.chop
-   #chop removes last character
-   "string."
+  string.chop!
+  string.strip
+  #string.chop takes off last character, chop! returns same string
+  #string.strip removes white space
+  puts "#{string}"
+  #why we have an end after puts??
+  end
+end
+
+def take_a_number(array, person)
+  array.push(person)
+  number = array.length
+  puts "Welcome, #{person}. You are number #{number} in line."
+end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
 end
