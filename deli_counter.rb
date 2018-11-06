9ccb39b1c782d3310f904286ff0a4fca1b64167a
a lan <bluenowte97@gmail.com>
2018-11-06 00:01:53 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(people)
+  if people.length == 0
+    puts "The line is currently empty."
+
+  people.each do.with_index(1) do |name, index|
+    string = "The line is currently #{index}. #{name}",
+  end
+   string = string.chop
+   #chop removes last character
+   "string."
+end
